d2c0b3be4ee1fac3aaa8867d71a7ee4eace725eb
Saba <46930420+Saundalf@users.noreply.github.com>
2023-04-04 22:02:18 +0400

    Create meotxe.hpp

---
+++ OOP_HW_2/meotxe.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+class Meotxe
+{
+public:
+	Meotxe(int n = 3, int m = 3);
+	void print();
+	void function();
+private:
+	const int rows;
+	const int columns;
+	int array[3][3];
+};
+//s
